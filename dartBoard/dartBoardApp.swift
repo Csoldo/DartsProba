207d008fc20b6435d149d15948b47bd2d0d89c95
Nagy Boldizsár <nagyboldi2000@gmail.com>
2021-01-31 18:49:00 +0100

    Initial Commit

---
+++ dartBoard/dartBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  dartBoardApp.swift
+//  dartBoard
+//
+//  Created by Nagy Boldizsár on 2021. 01. 31..
+//
+
+import SwiftUI
+
+@main
+struct dartBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
